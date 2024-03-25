2c04e015d154cc9d56a82a05ab4483a57dcd8b74
PstormQT <131224632+PstormQT@users.noreply.github.com>
2024-03-25 13:01:35 -0400

    Try python Json parse

--- BGguessinggame/UpdatedGameMachenic/mangadexApiFetch.py
+++ BGguessinggame/UpdatedGameMachenic/mangadexApiFetch.py
@@ -18,20 +18,22 @@ class dataFetch:
         
     def dataFetching(self):
         self.mangadata = requests.get(f"{self.BASE_URL}/manga", params = self.fetchParam)
-        self.mangadata = self.mangadata.json()["data"]
+        self.mangadata = self.mangadata.json()
 
     
     def jsonDump(self):
         openOutFile = open(self.outFileFull,"w")
         json.dump(self.mangadata, openOutFile,indent=6)
-        openOutFile.close()
         
     def getReqData(self):
-        with open(self.outFileFull) as dataFile: 
-            data = json.loads(dataFile)
-            
-            
-
+        with open(self.outFileFull, "r") as dataFile:
+            data = json.load(dataFile)
+            for manga in data["data"]:
+                shortmangadata = list()
+                shortmangadata.append((manga["id"]))
+                title = list(manga["attributes"]["title"])[0]
+                shortmangadata.append((title))
+                print(shortmangadata)
 def test():
     testcase = dataFetch()
     testcase.dataFetching()
