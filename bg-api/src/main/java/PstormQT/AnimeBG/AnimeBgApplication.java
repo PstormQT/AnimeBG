fdd402519d2a2ef21b7ca11c1e53e86332652186
PstormQT <131224632+PstormQT@users.noreply.github.com>
2025-02-17 13:24:47 -0500

    Init the main API

---
+++ bg-api/src/main/java/PstormQT/AnimeBG/AnimeBgApplication.java
@@ -0,0 +1,13 @@
+package PstormQT.AnimeBG;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AnimeBgApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AnimeBgApplication.class, args);
+	}
+
+}
---
+++ bg-api/src/test/java/PstormQT/AnimeBG/AnimeBgApplicationTests.java
@@ -0,0 +1,13 @@
+package PstormQT.AnimeBG;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AnimeBgApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
